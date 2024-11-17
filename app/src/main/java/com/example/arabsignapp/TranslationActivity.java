b12d2a9d3fea7a7bbc2fc02478b84cd70bad811e
Azzam4268 <azzam4268@gmail.com>
2024-11-17 18:38:06 +0300

    Add translate function for ASL after the sentence complete

--- app/src/main/java/com/example/arabsignapp/TranslationActivity.java
+++ app/src/main/java/com/example/arabsignapp/TranslationActivity.java
@@ -28,11 +28,14 @@ import com.google.common.util.concurrent.ListenableFuture;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.InetSocketAddress;
@@ -42,6 +45,15 @@ import java.util.LinkedHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.MediaType;
+
 
 public class TranslationActivity extends AppCompatActivity {
 
@@ -307,6 +319,9 @@ public class TranslationActivity extends AppCompatActivity {
             Log.d("CONDITION", String.valueOf(condition));
             if (condition){
                 sentence.add(translateText);
+                if (!arabic_mode) {
+                    translateToArabic(translateText);
+                }
                 translateText = "";
                 Log.d("SENTENCENEW", sentence.toString());
             }
@@ -442,6 +457,54 @@ public class TranslationActivity extends AppCompatActivity {
                 translateText = translateText.substring(0, translateText.length() - 1);
             }
         }
+    }
+
+    private void translateToArabic(String englishWord) {
+        String apiKey = "AIzaSyDtibFl4Vs_DCsRaDtsGlMegtpbn0XbBlY";
+        String url = "https://translation.googleapis.com/language/translate/v2?key=" + apiKey;
+
+        String jsonBody = "{ \"q\": \"" + englishWord + "\", \"source\": \"en\", \"target\": \"ar\", \"format\": \"text\" }";
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(jsonBody, MediaType.get("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String responseBody = response.body().string();
+                    String translatedText = parseTranslation(responseBody);
+                    runOnUiThread(() -> {
+                        translateView.setText(translatedText);
+                    });
+                }
+            }
+            private String parseTranslation(String responseBody) {
+                try {
+                    JSONObject jsonObject = new JSONObject(responseBody);
+
+                    return jsonObject.getJSONObject("data")
+                            .getJSONArray("translations")
+                            .getJSONObject(0)
+                            .getString("translatedText");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return "Error in translation";
+                }
+            }
+
+        });
+
+
     }
 
 
